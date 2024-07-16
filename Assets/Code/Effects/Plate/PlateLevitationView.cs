ac58bfd0a5ad3280992ee4a71cf793eeb8b87e2f
ladislao2v <vlad.khobian9@gmail.com>
2024-07-16 19:42:54 +0300

    Dotwwen add

--- Assets/Code/Effects/Plate/PlateLevitationView.cs
+++ Assets/Code/Effects/Plate/PlateLevitationView.cs
@@ -1,13 +1,30 @@
 ﻿using System;
+using DG.Tweening;
+using DG.Tweening.Core;
+using DG.Tweening.Plugins.Options;
 using UnityEngine;
 
 namespace Code.Effects.Plate
 {
     public class PlateLevitationView : MonoBehaviour
     {
-        private void Start()
+        [SerializeField] private float _force;
+        [SerializeField] private int _vibrato;
+        [SerializeField] private float _duration;
+        
+        private Sequence _sequence;
+        
+
+        private void OnEnable()
         {
-            
+            _sequence = DOTween.Sequence();
+
+            _sequence
+                .Append(transform.DOShakePosition(_duration, _force, _vibrato))
+                .SetLoops(-1);
         }
+
+        private void OnDisable() => 
+            _sequence.Kill();
     }
 }
